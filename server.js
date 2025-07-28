dbcdee9bb9b2a32486fc5da33307e9210a666640
Bryan <bmac929@yahoo.com>
2025-07-28 11:32:50 -0500

    deploying server2

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ const serviceAccount = JSON.parse(
     process.env.FIREBASE_CONFIG
   );
 
+serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
